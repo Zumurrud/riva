23dfce1ca43327e5d97475937e143f93e0805f77
Khulud Zumurrud <unsociable.catgirl@gmail.com>
2026-09-17 16:58:57 -0600

    Gave doeverything.py the ability to update source tables

--- datascript/doeverything.py
+++ datascript/doeverything.py
@@ -1,8 +1,20 @@
+import argparse
 import grabfiles
 import transformer
-from utils import load_json
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--update-sources",
+        "-u",
+        action="store_true",
+        help="Update the cached input tables before doing anything else.",
+    )
+    args = parser.parse_args()
+
+    if args.update_sources:
+        grabfiles.grab_tables()
+
     transformer.run_transformer()
     grabfiles.grab_avatars()
     transformer.process_images()
